8a18be66faaab97c99d1a4a465e0d6079b118b1d
Surya Vamsi <34418874+SuryaVamsiKK@users.noreply.github.com>
2019-03-14 15:38:42 +0530

    FIRST TOUCH DOWN
    working gravity with player touchdown

---
+++ Assets/Scripts/Planetry Shape and Colors/Tools/Attractor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    const float G = 667.4f;
+
+    public Rigidbody rb;
+
+    void FixedUpdate()
+    {
+        Rigidbody[] objects = FindObjectsOfType<Rigidbody>();
+        foreach (Rigidbody rb in objects)
+        {
+            if (rb != this.rb)
+            {
+                Attract(rb);
+            }
+        }
+    }
+
+    void Attract(Rigidbody objToAttract)
+    {
+        Vector3 direction = rb.position - objToAttract.position;
+        float disance = direction.magnitude;
+
+        float forceMagnitude = (rb.mass * objToAttract.mass) / Mathf.Pow(disance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        objToAttract.AddForce(force);
+    }
+
+}
